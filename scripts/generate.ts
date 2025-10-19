da047224c5263ec215ec470c194bfc380846a6b0
神代綺凛 <i@loli.best>
2025-10-19 21:27:49 +0800

    fix: conflict

--- scripts/generate.ts
+++ scripts/generate.ts
@@ -1,5 +1,6 @@
 import { readdir, readFile, writeFile } from 'node:fs/promises';
 import { join } from 'node:path';
+import { pull } from 'es-toolkit';
 import { execAsync } from './utils/exec';
 import { FbsParser } from './utils/fbs';
 
@@ -7,8 +8,12 @@ const FBS_DIR = 'OpenArknightsFBS/FBS';
 
 const files = (await readdir(FBS_DIR)).sort();
 
+// 有些文件类型存在冲突（且 OpenArknightsFBS 未修复），因此提前生成，让后面的文件覆盖，避免影响其他文件
+const conflictFiles = ['sandbox_table.fbs'];
+const genFiles = [...conflictFiles, ...pull([...files], conflictFiles)];
+
 await execAsync(
-  `flatc --ts --gen-object-api --no-warnings -o ${join('src', 'fbs')} ${files.map(file => join(FBS_DIR, file)).join(' ')}`,
+  `flatc --ts --gen-object-api --no-warnings -o ${join('src', 'fbs')} ${genFiles.map(file => join(FBS_DIR, file)).join(' ')}`,
 );
 
 const parsers = await Promise.all(
